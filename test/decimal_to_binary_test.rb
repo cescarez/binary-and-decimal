d70c8e40cc34efaa399a1af1fb93e3e95546872f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-09 14:05:31 -0700

    renamed and fixed decimal_to_binary test file

--- test/decimal_to_binary_test.rb
+++ test/decimal_to_binary_test.rb
@@ -7,24 +7,24 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 describe "binary to decimal" do
   it "From 153 to 10011011" do
     decimal = 155
-    # expected_binary_array = [1, 0, 0, 1, 1, 0, 1, 1]
-    expected_binary_array = decimal.to_s(2)
+    expected_binary_array = [1, 0, 0, 1, 1, 0, 1, 1]
+    # expected_binary_array = decimal.to_s(2)
 
     expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 
   it "From 45 to 00101101" do
     decimal = 45
-    # expected_binary_array = [0, 0, 1, 0, 1, 1, 0, 1]
-    expected_binary_array = decimal.to_s(2)
+    expected_binary_array = [0, 0, 1, 0, 1, 1, 0, 1]
+    # expected_binary_array = decimal.to_s(2)
 
     expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
 
   it "From 129 to 10000001" do
     decimal = 129
-    # expected_binary_array = [1, 0, 0, 0, 0, 0, 0, 1]
-    expected_binary_array = decimal.to_s(2)
+    expected_binary_array = [1, 0, 0, 0, 0, 0, 0, 1]
+    # expected_binary_array = decimal.to_s(2)
 
     expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
@@ -32,6 +32,7 @@ describe "binary to decimal" do
   it "From random decimal to binary" do
     num_bits = 8
     max_decimal = 0
+    random_binary_array = []
     num_bits.times do |i|
       random_binary_array << rand(0..1)
       max_decimal += (2 ** (((num_bits - 1)) - i))
@@ -41,3 +42,4 @@ describe "binary to decimal" do
 
     expect(decimal_to_binary(decimal)).must_equal expected_binary_array
   end
+end
